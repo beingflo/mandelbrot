bf3e021e364008e37b6348076948e84acd22b7b6
Florian Marending <floriama@student.ethz.ch>
2017-01-28 16:46:44 +0100

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,17 @@ use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
 use glium::{ DisplayBuild, Surface, glutin };
-
+use glium::backend::glutin_backend::GlutinFacade;
 
 fn main() {
     let display = glutin::WindowBuilder::new().build_glium().unwrap();
 
-    let p1 = Vertex { position: [-1.0, -1.0] };
-    let p2 = Vertex { position: [-1.0, 1.0] };
-    let p3 = Vertex { position: [1.0, -1.0] };
-    let p4 = Vertex { position: [1.0, -1.0] };
-    let p5 = Vertex { position: [1.0, 1.0] };
-    let p6 = Vertex { position: [-1.0, 1.0] };
-    let data = vec![p1, p2, p3, p4, p5, p6];
+    let data = get_vertex_data();
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &data).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-    let vertex_shader_src = load_shader("src/shader/vert_shader.glslv");
-    let fragment_shader_src = load_shader("src/shader/frag_shader.glslf");
-
-    let program = glium::Program::from_source(&display, &vertex_shader_src,
-                                              &fragment_shader_src, None).unwrap();
+    let program = make_program(&display);
 
     let mut num_iterations = 100;
     let mut x_shift: f32 = -0.7;
@@ -34,7 +24,6 @@ fn main() {
 
     loop {
         let mut target = display.draw();
-
 
         target.clear_color(1.0, 1.0, 1.0, 0.0);
         target.draw(&vertex_buffer, &indices, &program,
@@ -62,6 +51,26 @@ fn main() {
         }
     }
 }
+
+fn get_vertex_data() -> Vec<Vertex> {
+    let p1 = Vertex { position: [-1.0, -1.0] };
+    let p2 = Vertex { position: [-1.0, 1.0] };
+    let p3 = Vertex { position: [1.0, -1.0] };
+    let p4 = Vertex { position: [1.0, -1.0] };
+    let p5 = Vertex { position: [1.0, 1.0] };
+    let p6 = Vertex { position: [-1.0, 1.0] };
+
+    vec![p1, p2, p3, p4, p5, p6]
+}
+
+fn make_program(display: &GlutinFacade) -> glium::Program {
+    let vertex_shader_src = load_shader("src/shader/vert_shader.glslv");
+    let fragment_shader_src = load_shader("src/shader/frag_shader.glslf");
+
+    glium::Program::from_source(display, &vertex_shader_src,
+                                &fragment_shader_src, None).unwrap()
+}
+
 
 fn load_shader(file: &str) -> String {
     let path = Path::new(file);
